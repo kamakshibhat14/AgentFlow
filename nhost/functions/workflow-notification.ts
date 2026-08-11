dcd91ff74b81fe121dd723ab2423c8b99bce522e
Kamakshi Bhat <kamakshibhat9@gmail.com>
2026-08-11 14:59:17 +0530

    add workflow notification event handler

---
+++ nhost/functions/workflow-notification.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+
+export default async (req: Request, res: Response) => {
+  console.log("Workflow notification event:", JSON.stringify(req.body));
+
+  return res.status(200).json({
+    success: true,
+    message: "Workflow notification received",
+  });
+};
